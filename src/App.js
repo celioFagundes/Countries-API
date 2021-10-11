996ef7125a7a848db1f33ee1469bd563c8414dda
celioFagundes <celiofagundes30@gmail.com>
2021-10-11 17:32:56 -0300

    changed browser router to hash router

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import CountriesContext from './components/ContextCountries/context'
 import Header from './components/Header/Header';
 import HomePage from './components/HomePage/HomePage';
 import DetailPage from './components/DetailPage/DetailPage';
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import {HashRouter, Switch, Route} from 'react-router-dom'
 function App() {
 
   const [theme,setTheme] = useState(dark)
@@ -18,18 +18,18 @@ function App() {
   }
   return (
     <ThemeProvider theme = {theme}>
-      <BrowserRouter>
+      <HashRouter>
         <div className="App">
           <GlobalStyle/> 
           <Header toggleTheme = {handleTheme} theme = {theme.title}/>
           <Switch>
             <CountriesContext>
-              <Route exact path="Countries-API/" render={(props) => <HomePage {...props} theme ={theme.title}/>} />
-              <Route exact path="Countries-API/details/:id" render={(props) => <DetailPage {...props} theme ={theme.title}/>} />
+              <Route exact path="/" render={(props) => <HomePage {...props} theme ={theme.title}/>} />
+              <Route exact path="/details/:id" render={(props) => <DetailPage {...props} theme ={theme.title}/>} />
             </CountriesContext>
           </Switch>
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   );
 }
